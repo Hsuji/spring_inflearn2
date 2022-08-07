782b0a36ab9c4bebaf3b06453e7aa0c4495c77e1
Hsuji <hsuji.work@gmail.com>
2022-08-07 12:10:43 +0900

    XML설정 형식

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        // resources 밑에 있는 appConfig.xml 설정파일 사용
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
