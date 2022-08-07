2e4740bcf1d973d96580a0a32c6a2349939d3b73
Hsuji <hsuji.work@gmail.com>
2022-08-07 11:12:17 +0900

    자바 -> 스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,28 +1,37 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * 애플리케이션 설정 정보
+  */
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepsitory());
     }
 
-    private MemoryMemberRepository memberRepsitory() {
+    @Bean
+    public MemoryMemberRepository memberRepsitory() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepsitory(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // 정액할인
 //        return new FixDiscountPolicy();
